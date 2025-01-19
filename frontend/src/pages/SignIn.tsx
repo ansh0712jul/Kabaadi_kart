4a30d75ea4a01e7a0fc292f3f62dfd75a05f42cd
Ansh agrawal <agrawalansh624@gmail.com>
2025-01-20 00:43:56 +0530

    Integrate Zod validation

--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import { useState } from 'react'
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card"
@@ -7,8 +5,43 @@ import { Input } from "@/components/ui/input"
 import { Link } from 'react-router-dom'
 import {  User, Lock } from 'lucide-react'
 import "../App.css"
+import {useForm} from "react-hook-form";
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
 
+// schema for validation 
+const signInSchema = z.object({
+  email: z
+  .string()
+  .email({message:"Invalid email format"})
+  .nonempty("email is required"),
+  password: z
+  .string()
+  .nonempty("Password is required")
+
+})
+
+
+// type defintion of form data
+
+type SignInFormData = z.infer<typeof signInSchema>
 const SignIn = () => {
+ 
+  // initializing the  react hook form with zod Resolver
+  const { 
+    register, 
+    handleSubmit, 
+    formState: { errors } } = useForm<SignInFormData>({
+      resolver: zodResolver(signInSchema),
+  })
+
+  // handle form submission
+
+  const handleLogin = (data: SignInFormData) => {
+    console.log(data);
+  }
+
+
   const [isHovered, setIsHovered] = useState(false)
 
   return (
@@ -23,23 +56,42 @@ const SignIn = () => {
           <p className="text-center text-lg text-gray-200 mt-2">Sign in to your account</p>
         </CardHeader>
         <CardContent>
-          <form className="space-y-6">
+          <form 
+          onSubmit={handleSubmit((handleLogin)) }
+          className="space-y-6">
             <div className="relative">
-              <User className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-600" />
+              <User className={`absolute left-3  ${errors.email ? 'top-1/3' : "top-1/2"} transform -translate-y-1/2 text-gray-600`}/>
               <Input
-                className="pl-10 w-full text-lg text-white bg-white/20 border-0 rounded-lg py-3 px-10 focus:outline-none focus:ring-2 focus:ring-white/50 placeholder-gray-300"
+                className={`pl-10 w-full text-lg text-white bg-white/20 border-0 rounded-lg py-3 px-10 focus:outline-none focus:ring-2 focus:ring-white/50 placeholder-gray-300`}
                 type="text"
                 placeholder="username or email"
+                {
+                  ...register("email")
+                }
               />
+              {
+                errors.email && (
+                  <p className="text-red-500 text-sm mt-1">{errors.email.message}</p>
+                )
+              }
             </div>
 
             <div className="relative">
-              <Lock className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-600" />
+              <Lock className={`absolute left-3  ${errors.password ? 'top-1/3' : "top-1/2"} transform -translate-y-1/2 text-gray-600`} />
               <Input
                 className="pl-10 w-full text-lg text-white bg-white/20 border-0 rounded-lg py-3 px-10 focus:outline-none focus:ring-2 focus:ring-white/50 placeholder-gray-300"
                 type="password"
                 placeholder="Your password"
+                {
+                  ...register("password")
+                }
               />
+
+              {
+                errors.password && (
+                  <p className="text-red-500 text-sm mt-1">{errors.password.message}</p>
+                )
+              }
             </div>
 
             <Button className="w-full text-xl py-6 mt-4 bg-gradient-to-r from-teal-400 to-blue-500 text-white rounded-lg hover:from-teal-500 hover:to-blue-600 focus:outline-none focus:ring-2 focus:ring-teal-500 transition-all duration-300 ease-in-out transform hover:scale-105">
--- frontend/src/pages/SignUp.tsx
+++ frontend/src/pages/SignUp.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import { useState } from 'react'
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card"
@@ -7,8 +5,54 @@ import { Input } from "@/components/ui/input"
 import { Link } from 'react-router-dom'
 import { Mail, User, Lock } from 'lucide-react'
 import "../App.css"
+import { useForm} from "react-hook-form";
+import {zodResolver} from "@hookform/resolvers/zod";
+import { z } from "zod";
+
+
+// schema for zod validation 
+const signUpSchema = z.object({
+  username : z
+        .string()
+        .min(3, { message: "Username must be at least 3 characters long" })
+        .nonempty({ message: "Username is required" })
+        .regex(/^[a-zA-Z0-9_]+$/, "Username must only contain letters, numbers, and underscores"),
+        email: z
+        .string()
+        .email({message:"Invalid email format"})
+        .nonempty("email is required"),
+        password: z.
+        string()
+        .min(4, {message:"Password must be at least 4 characters long"})
+        .max(12, {message:"Password must be at most 12 characters long"})
+        .nonempty("Password is required")
+})
+
+
+// type definiton of form data
+type SignUpFormData = z.infer<typeof signUpSchema>
+
 
 const SignUp = () => {
+
+  
+  // initializing the  react hook form with zod Resolver
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    } = useForm<SignUpFormData>({
+    resolver: zodResolver(signUpSchema),
+    
+  });
+
+
+  // handle form submission
+  const onSubmit = (data: SignUpFormData) => {
+    console.log(data)
+  }
+
+
   const [isHovered, setIsHovered] = useState(false)
 
   return (
@@ -23,32 +67,53 @@ const SignUp = () => {
           <p className="text-center text-lg text-gray-200 mt-2">Join us and start your journey!</p>
         </CardHeader>
         <CardContent>
-          <form className="space-y-6">
+          <form 
+          onSubmit={handleSubmit(onSubmit) }
+          className="space-y-6">
             <div className="relative">
-              <Mail className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-600" />
+              <Mail className={`absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-600 ${errors.email ? 'top-1/3' : 'top-1/2'}`} />
               <Input
                 className=" pl-10 w-full text-lg text-white bg-white/20 border-0 rounded-lg py-3 px-10 focus:outline-none focus:ring-2 focus:ring-white/50 placeholder-gray-300"
-                type="email"
+                type="text"
                 placeholder="Enter your email"
+                {...register("email")}
               />
+
+              {
+                errors.email && <p className="text-sm text-red-500 mt-1">{errors.email.message}</p>
+              }
             </div>
 
             <div className="relative">
-              <User className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-600" />
+              <User className={`absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-600 ${errors.username ? 'top-1/3' : 'top-1/2'}`} />
               <Input
                 className="pl-10 w-full text-lg text-white bg-white/20 border-0 rounded-lg py-3 px-10 focus:outline-none focus:ring-2 focus:ring-white/50 placeholder-gray-300"
                 type="text"
                 placeholder="Choose a username"
+                {...register("username")}
               />
+
+              {
+                errors.username && <p className="text-sm text-red-500 mt-1">{errors.username.message}</p>
+              }
+
+
             </div>
 
             <div className="relative">
-              <Lock className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-600" />
+              <Lock className={`absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-600 ${errors.password ? 'top-1/3' : 'top-1/2'}`} />
               <Input
                 className="pl-10 w-full text-lg text-white bg-white/20 border-0 rounded-lg py-3 px-10 focus:outline-none focus:ring-2 focus:ring-white/50 placeholder-gray-300"
                 type="password"
                 placeholder="Create a password"
+                {...register("password")}
               />
+              
+              {
+                errors.password && <p className="text-sm text-red-500 mt-1">{errors.password.message}</p>
+              }
+
+
             </div>
 
             <Button className="w-full text-xl py-6 mt-4 bg-gradient-to-r from-teal-400 to-blue-500 text-white rounded-lg hover:from-teal-500 hover:to-blue-600 focus:outline-none focus:ring-2 focus:ring-teal-500 transition-all duration-300 ease-in-out transform hover:scale-105">
