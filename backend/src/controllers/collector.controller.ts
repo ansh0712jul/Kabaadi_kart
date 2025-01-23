ca74059aa6dc26feda3b978d392ccb2712798018
Ansh agrawal <agrawalansh624@gmail.com>
2025-01-24 00:32:37 +0530

    logout endpoint

--- backend/src/controllers/collector.controller.ts
+++ backend/src/controllers/collector.controller.ts
@@ -143,3 +143,35 @@ export const loginCollector = asyncHandler(async(req:Request , res:Response) => 
         )
     )
 })
+
+// logout endpoint 
+
+export const logoutCollector = asyncHandler(async(req:AuthenticatedRequest , res:Response) => {
+
+    if(!req.user){
+        throw new ApiError(401 , "user is not authenticated");
+    }
+
+    await Collector.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set : {
+                refreshToken : undefined
+            }
+        },
+        {new : true}
+    )
+
+    const options = {
+        httpOnly : true,
+        secure : true,
+    }
+
+    return res
+    .status(200)
+    .clearCookie("accessToken" , options)
+    .clearCookie("refreshToken" , options)
+    .json(
+        new ApiResponse(200,{} , "user logged out successfully")
+    )
+})
--- backend/src/routes/collector.routes.ts
+++ backend/src/routes/collector.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { loginCollector, registerCollector } from "../controllers/collector.controller";
-
+import { loginCollector, logoutCollector, registerCollector } from "../controllers/collector.controller";
+import { verifyCollectorJwt } from "../middleware/authC.middleware";
 const router = Router();
 
 router.route("/sign-up").post(registerCollector);
 router.route("/sign-in").post(loginCollector);
+router.route("/logout").post(verifyCollectorJwt ,logoutCollector);
 
 
 export default router
